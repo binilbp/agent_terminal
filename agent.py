9afbfe459e289b70fba7bda885e680b128742b07
binilbp <biniloffcl@gmail.com>
2026-01-07 15:34:30 +0530

    Setup base agentstate

--- agent.py
+++ agent.py
@@ -1,18 +1,25 @@
-from typing import Annotated
-from typing_extensions import TypedDict
+from typing_extensions import TypedDict, Annotated
 
 from langchain.tools import tool
 from langchain_ollama import ChatOllama
 from langgraph.graph import StateGraph, START, END
 
-class AgentState(TypedDict):
-    goal: str
-    command_generated: str
-    exit_code: int
-    user_approved: bool = False
-    sanity_check_passed: bool = False
+# class AgentState(TypedDict):
+#     goal: str
+#     command_generated: str
+#     exit_code: int
+#     user_approved: bool = False
+#     sanity_check_passed: bool = False
 
 llm = ChatOllama(model = "llama3.2", temperature=0)
+
+class AgentState(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+
+
+
 
 
 
