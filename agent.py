95de2543f68bebeb0ab00bf72703a43bf7808cd8
binilbp <biniloffcl@gmail.com>
2026-01-08 18:39:37 +0530

    Add Tool, Tool output scheme, Model definitions and Few shot prompting

--- agent.py
+++ agent.py
@@ -1,10 +1,12 @@
 from typing_extensions import TypedDict, Annotated
+from pydantic import BaseModel, Field
 
-# from langchain.tools import tool
+from langchain.tools import tool
 from langchain_ollama import ChatOllama
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
 
 # class AgentState(TypedDict):
 #     goal: str
@@ -13,35 +15,78 @@ from langgraph.graph.message import add_messages
 #     user_approved: bool = False
 #     sanity_check_passed: bool = False
 
-big_llm = ChatOllama(model = "qwen2.5-coder:3b", temperature=0)
 
+
+# Schemas
+class LinuxCommand(BaseModel):
+    """Schema to force the LLM to output ONLY the command."""
+    command: str = Field(description="The pure linux shell command. No markdown, no explanations.")
+
+
+
+# Tools
+@tool
+def generate_command(query: str) -> str:
+    """
+    Generate a Linux shell command.
+    Use this tool when the user asks for a specific terminal operation.
+    
+    Args:
+        query: The user's request (e.g. "install calculator")
+    """    
+    prompt = f"return the command for executing the user query on ubuntu: {query}"
+
+    print("[info]: kutti llm running...")
+    tool_output = structured_small_llm.invoke(prompt)
+    return tool_output.command
+
+tools_list = [generate_command]
+
+# State Defining
 class AgentState(TypedDict):
     messages: Annotated[list, add_messages]
+
+
+
+# Model Defining
+small_llm = ChatOllama(model = "qwen2.5-coder:3b", temperature=0)
+structured_small_llm = small_llm.with_structured_output(LinuxCommand)
+big_llm = ChatOllama(model = "llama3.1:8b", temperature=0)
+big_llm = big_llm.bind_tools(tools_list)
+
 
 
 # Nodes
 def big_agent(state: AgentState):
     messages = state["messages"]
-
-    response = big_llm.invoke(messages)
-
+    sys_message = SystemMessage(content="""
+    You are a helpful assistant. 
+        - If the user just says "hi" or converses normally, reply naturally. 
+        - ONLY call the 'generate_command' tool if the user explicitly asks for a Linux terminal command.
+    """)    
+    print("[INFO]: paapa llm running...")
+    response = big_llm.invoke([sys_message] + messages)
     return {"messages": [response]}
+
 
 
 # Building the Graph
 graph_builder = StateGraph(AgentState)
-
 graph_builder.add_node("big_agent", big_agent)
+graph_builder.add_node("tools", ToolNode(tools_list))
+
 graph_builder.add_edge(START, "big_agent")
-graph_builder.add_edge("big_agent",END)
+graph_builder.add_conditional_edges("big_agent",tools_condition)
+graph_builder.add_edge("tools", "big_agent")
 
 graph=graph_builder.compile()
+
 
 
 # Loop Function
 def run_agent():
     while True:
-        user_input = input("User: ")
+        user_input = input("\nUSER: ")
 
         #condition to break the while loop/ to exit
         if user_input.lower() in ["quit", "exit"]:
@@ -52,17 +97,18 @@ def run_agent():
                 stream_mode="values"
         )
        
+        print("\n")
         for event in events:
             if "messages" in event:
                 last_message = event["messages"][-1]
                 if last_message.type == "ai":
                     if last_message.tool_calls:
-                        print("f Tool Called: {last_message.tool_calls[0]['name']} ")
+                        print(f"[INFO]: Tool Called {last_message.tool_calls[0]['name']} ")
                     else:
-                        print(f"Agent: {last_message.content}")
+                        print(f"\nAGENT: {last_message.content}\n")
 
                 elif last_message.type == "tool":
-                    print(f"Tool Output: {last_message.content}")
+                    print(f"[INFO]: Tool Output {last_message.content}")
 
         
 
