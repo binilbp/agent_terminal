a9945d83863b95c6d2785b61c92cb8d2e95bf878
binilbp <biniloffcl@gmail.com>
2026-01-10 08:40:59 +0000

    Create a defualt state

---
+++ Agent/utils/state.py
@@ -0,0 +1,10 @@
+# this file is used to define the state used by the agent
+
+
+from typing_extensions import TypedDict, Annotated
+from langgraph.graph.message import add_messages
+
+
+
+class AgentState(TypedDict):
+    messages: Annotated[list, add_messages]
