57d5f548f9567c50c6e4fcffd585f79731855f06
binilbp <biniloffcl@gmail.com>
2026-01-12 13:07:44 +0530

    Add test_print_state node; Modify is_sys_ass_req node to take user_input from state

--- Agent/utils/nodes.py
+++ Agent/utils/nodes.py
@@ -1,6 +1,10 @@
 # this file contains all the node definitions used by the langgraph
+# nodes beggining with "test" are only used for test purposes
 
 from Agent.utils.llm import get_classifier_llm
+from Agent.utils.state import AgentState
+from pprint import pformat
+
 
 
 # llms
@@ -8,12 +12,24 @@ classifier_llm = get_classifier_llm()
 
 
 
-def is_sys_ass_req(state): 
+def is_sys_ass_req(state: AgentState): 
 
-    query = "find the nvidia driver version insalled"
+    query = state["messages"] 
+    try:
+        print(f'[STATE INFO]: \n{pformat(state)}')
 
-    print("[INFO]: identifying user request type")
-    result = classifier_llm.invoke(query)
-    print(f'[AGENT]: {result}')
+        print("[INFO]: identifying user request type")
+        result = classifier_llm.invoke(query)
+        print(f'[AGENT]: {result}')
+        
+        return { 'is_sys_ass_req': result.requires_linux_assistance }
+    except Exception as e:
+        print("[WARN]: classifier failed, defaulting to False")
+        print(e)
+        return { 'is_sys_ass_req': False}
 
 
+
+from pprint import pformat
+def test_print_state(state: AgentState):
+    print(f'\nCurrent State: {pformat(state)}\n')
