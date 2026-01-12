033b1b5c9d300edfa4a9181e2f0c593b2bf4e79e
binilbp <biniloffcl@gmail.com>
2026-01-12 13:10:53 +0530

    Create graph.py; Add get_graph() to return graph

---
+++ Agent/graph.py
@@ -0,0 +1,22 @@
+# this file contains function to build the agent graph and return it
+
+
+
+from langgraph.graph import StateGraph, START, END
+from Agent.utils.state import AgentState 
+from Agent.utils.nodes import is_sys_ass_req, test_print_state
+
+
+
+def get_graph():
+    graph_builder = StateGraph(AgentState)
+    graph_builder.add_node("is_sys_ass_req", is_sys_ass_req )
+
+    #test node
+    graph_builder.add_node("print_state", test_print_state)
+
+    graph_builder.add_edge( START, "is_sys_ass_req" )
+    graph_builder.add_edge( "is_sys_ass_req", "print_state" )
+    graph_builder.add_edge( "print_state", END )
+
+    return graph_builder.compile()
--- Agent/utils/state.py
+++ Agent/utils/state.py
@@ -8,3 +8,5 @@ from langgraph.graph.message import add_messages
 
 class AgentState(TypedDict):
     messages: Annotated[list, add_messages]
+    is_sys_ass_req: bool
+
