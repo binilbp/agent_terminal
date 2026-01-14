4e3b7b952c885faa01c5006a7550810825a768c8
binilbp <biniloffcl@gmail.com>
2026-01-14 19:41:33 +0530

    move while loop and user input from main.py to Agent/agent.py

--- Agent/agent.py
+++ Agent/agent.py
@@ -11,22 +11,29 @@ graph = get_graph()
 
 
 
-def run_agent(user_input):
-    events = graph.stream(
-            {"messages":[HumanMessage(content=user_input)]},
-            stream_mode="values"
-    )
-   
-    for event in events:
-        if "messages" in event:
-            last_message = event["messages"][-1]
-            if last_message.type == "ai":
-                if last_message.tool_calls:
-                    print(f"[INFO]: Tool Called: {last_message.tool_calls[0]['name']} ")
-                else:
-                    print(f"\nAGENT: {last_message.content}\n")
+def run_agent():
+    while True:
+        user_input = input("\nUSER: ")
 
-            elif last_message.type == "tool":
-                print(f"[INFO]: Tool Output: {last_message.content}")
+        #condition to break the while loop/ to exit
+        if user_input.lower() in ["quit", "exit"]:
+            break
+
+        events = graph.stream(
+                    {"messages":[HumanMessage(content=user_input)]},
+                    stream_mode="values"
+        )
+           
+        for event in events:
+            if "messages" in event:
+                last_message = event["messages"][-1]
+                if last_message.type == "ai":
+                    if last_message.tool_calls:
+                        print(f"[INFO]: Tool Called: {last_message.tool_calls[0]['name']} ")
+                    else:
+                        print(f"\nAGENT: {last_message.content}\n")
+
+                elif last_message.type == "tool":
+                    print(f"[INFO]: Tool Output: {last_message.content}")
 
 
--- main.py
+++ main.py
@@ -5,14 +5,7 @@ def main():
     # print(SETTINGS.classifier_llm.service)
     print(f'SETTINGS Applied:\n {SETTINGS.model_dump_json(indent=4)}\n')
     
-    while True:
-        user_input = input("\nUSER: ")
-
-        #condition to break the while loop/ to exit
-        if user_input.lower() in ["quit", "exit"]:
-            break
-
-        run_agent(user_input)
+    run_agent()
 
 
 if __name__ == "__main__":
