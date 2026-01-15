35d2d56b3dddfcf11ae307f4998fd25290e191bd
binilbp <biniloffcl@gmail.com>
2026-01-16 01:01:25 +0530

    Add textual requirement; Modify main to run textual ui

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from config.settings import SETTINGS
 from Agent.agent import run_agent
+from TUI.ui import app
+
+
 
 def main():
     # print(SETTINGS.classifier_llm.service)
-    if SETTINGS.debug:
-        print(f'SETTINGS Applied:\n {SETTINGS.model_dump_json(indent=4)}\n')
-    
-    print("Linquix Backend")
-    print("Type 'exit' for quit")
-    run_agent()
+    # if SETTINGS.debug:
+    #     print(f'SETTINGS Applied:\n {SETTINGS.model_dump_json(indent=4)}\n')
+    #
+    # print("Linquix Backend")
+    # print("Type 'exit' for quit")
+    # run_agent()
+    app.run()
 
 
 if __name__ == "__main__":
