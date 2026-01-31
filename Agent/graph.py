3556d971a3073e0e3800fcf8f04edd485cbfcb9a
binilbp <biniloffcl@gmail.com>
2026-01-31 20:18:01 +0530

    Add supervisor, commandgeneration and execution nodesto the graph;Add planning-supervisor edge

--- Agent/graph.py
+++ Agent/graph.py
@@ -4,7 +4,7 @@
 
 from langgraph.graph import StateGraph, START, END
 from Agent.utils.state import AgentState 
-from Agent.utils.nodes import classification, planning, out_of_scope, clarification, test_print_state
+from Agent.utils.nodes import classification, planning, out_of_scope, clarification, test_print_state, supervisor, command_generation, execution
 from langgraph.checkpoint.memory import MemorySaver
 
 
@@ -15,10 +15,14 @@ def get_graph():
     graph_builder.add_node( "planning", planning )
     graph_builder.add_node( "out_of_scope", out_of_scope )
     graph_builder.add_node( "clarification", clarification )
-
+    graph_builder.add_node( "supervisor", supervisor )
+    graph_builder.add_node( "command_generation", command_generation )
+    graph_builder.add_node( "execution", execution )
 
     graph_builder.add_edge( START, "classification" )
-    graph_builder.add_edge( "clarification", END )
+    graph_builder.add_edge( "planning", "supervisor")
+    graph_builder.add_edge( "command_generation", "supervisor" )
+    graph_builder.add_edge( "execution", END)
 
     memory = MemorySaver()
     return graph_builder.compile(checkpointer=memory)
